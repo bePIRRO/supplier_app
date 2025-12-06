292951f79904ed1d87aa878e5268fa76829a2c17
bePIRRO <manuel.pirro@gmail.com>
2025-12-06 13:51:05 +0100

    refactor: Format month array for better readability in date formatting

--- lib/screens/checkout/place_order_screen.dart
+++ lib/screens/checkout/place_order_screen.dart
@@ -504,7 +504,20 @@ class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
   }
 
   String _formatDate(DateTime date) {
-    final months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    final months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
     return '${months[date.month - 1]} ${date.day}, ${date.year}';
   }
 
